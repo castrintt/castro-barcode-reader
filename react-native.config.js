57f63f29a3ed6afe22400a3213913ff6295632d1
castro <igordc38@gmail.com>
2025-10-15 13:22:11 -0300

    Update AndroidManifest and package.json; add react-native.config.js for Android integration

---
+++ react-native.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dependency: {
+    platforms: {
+      android: {
+        packageImportPath:
+          "import com.castro.HoneywellBarcodeReader.HoneywellBarcodeReaderPackage;",
+        packageInstance: "new HoneywellBarcodeReaderPackage()",
+      },
+    },
+  },
+};
